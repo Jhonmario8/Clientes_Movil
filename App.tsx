2521fd721bbf1c39f4b075e6425b5bed247d4c0b
Jhonma <Jhonma1238@gmail.com>
2026-03-26 09:15:05 -0500

    sidebar desplegable en movil

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useWindowDimensions } from "react-native";
 
 import Splash from "./screens/splash";
 import Login from "./screens/login";
@@ -14,18 +15,24 @@ const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerMenu() {
+  const { width } = useWindowDimensions();
+
+ 
+  const isLargeScreen = width >= 768;
+
   return (
-   <Drawer.Navigator
-  drawerContent={(props) => <CustomDrawer {...props} />}
-  screenOptions={{
-    headerShown: false,
-    drawerType: "permanent", 
-    drawerStyle: {
-      width: 240,
-      backgroundColor: "#0F172A",
-    },
-  }}
->
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawer {...props} />}
+      screenOptions={{
+        headerShown: !isLargeScreen, 
+        drawerType: isLargeScreen ? "permanent" : "front", 
+        drawerStyle: {
+          width: isLargeScreen ? 240 : 220,
+          backgroundColor: "#0F172A",
+        },
+        overlayColor: "rgba(0,0,0,0.5)", 
+      }}
+    >
       <Drawer.Screen name="Inicio" component={Home} />
       <Drawer.Screen name="Clientes" component={Cliente} />
     </Drawer.Navigator>
